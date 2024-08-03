733e87fedc9f6a3f5ee478f932a9f20006bbf797
angjun04 <peteryoung04@naver.com>
2024-08-03 17:59:51 +0900

    login added userid

--- app/api/user/login/route.ts
+++ app/api/user/login/route.ts
@@ -14,7 +14,14 @@ export async function POST(req:Request) {
     const { data, error } = await supabase.auth.signInWithPassword({
         email: email,
         password: pw
-      });
-    if(error) return NextResponse.json({error:error},{status:500})
-    return NextResponse.json({data:data}, {status:200});
+    });
+    if(error) return NextResponse.json({error:error},{status:400})
+    let { data: user_id, error:userError } = await supabase
+    .from('userInfo')
+    .select('user_id')
+    .eq('email', email)
+    .single();
+    
+    if(userError) return NextResponse.json({error:error},{status:500})
+    return NextResponse.json({data:data, userId:user_id?.user_id}, {status:200});
 }
