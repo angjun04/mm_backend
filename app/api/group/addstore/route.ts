417a9218a5cd4b30be1840944cf3ba22c7d03cbf
angjun04 <peteryoung04@naver.com>
2024-08-03 10:39:26 +0900

    added delete api

--- app/api/group/addstore/route.ts
+++ app/api/group/addstore/route.ts
@@ -11,6 +11,16 @@ export async function POST(req:Request) {
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     );
+    const { data: existingGroup, error: checkError } = await supabase
+    .from('GroupStores')
+    .select('store_id')
+    .eq('group_id', group_id)
+    .eq('store_id', store_id)
+    .single();
+
+    if (checkError && checkError.code !== 'PGRST116') return NextResponse.json({ error: checkError.message }, { status: 500 });
+    if (existingGroup) return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+
     const { data:groupStore, error:err } = await supabase
     .from('GroupStores')
     .insert([
@@ -20,3 +30,23 @@ export async function POST(req:Request) {
     if(err) return NextResponse.json({error:err},{status:500})
     return NextResponse.json({data:groupStore}, {status:200});
 }
+
+export async function DELETE(req: Request) {
+    const { group_id, store_id } = await req.json();
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    );
+  
+    const { data, error } = await supabase
+      .from('GroupStores')
+      .delete()
+      .eq('group_id', group_id)
+      .eq('store_id', store_id);
+  
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+  
+    return NextResponse.json({ message: 'Group deleted successfully' }, { status: 200 });
+  }
